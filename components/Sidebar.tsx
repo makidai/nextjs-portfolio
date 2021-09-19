13e66cd09288c9e4002fc20a4d1c78f5f9802bc4
Ryodai Makino <davinci0327@icloud.com>
2021-09-19 16:41:28 +0900

    setup typescript and tailwindcss

---
+++ components/Sidebar.tsx
@@ -0,0 +1,11 @@
+const Sidebar = () => {
+	return (
+		<div>
+			<img src="/avatar.jpg" alt="user avatar"/>
+			<h3><span>Ryodai</span> Makino</h3>
+			<p>Web Developer</p>
+		</div>
+	)
+}
+
+export default Sidebar
---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+const index = () => {
+    return (
+        <div>
+            <h1>Main content</h1>
+        </div>
+    );
+};
+
+export default index;
