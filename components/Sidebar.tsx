4fedda842ba71d879204a9c2649cfcabf3fcb9c1
Ryodai Makino <davinci0327@icloud.com>
2021-09-20 22:56:57 +0900

    create Sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,11 +1,46 @@
-const Sidebar = () => {
-	return (
-		<div>
-			<img src="/avatar.jpg" alt="user avatar"/>
-			<h3><span>Ryodai</span> Makino</h3>
-			<p>Web Developer</p>
-		</div>
-	)
-}
+import { AiFillGithub } from "react-icons/ai";
+import { GoLocation } from "react-icons/go";
+import Image from "next/image";
 
-export default Sidebar
+const Sidebar = () => {
+    return (
+        <>
+            <Image
+                src="/avatar.jpg"
+                alt="avatar"
+                className="mx-auto border rounded-full "
+                height="128px"
+                width="128px"
+                layout="intrinsic"
+                quality="100"
+            />
+            <h3 className="my-4 text-3xl font-medium tracking-wider font-kaushan">
+                <span className="text-orange ">Ryodai</span> Makino
+            </h3>
+            <p className="px-2 py-1 my-3 bg-gray-200 rounded-full dark:bg-dark-200 dark:bg-black-500">
+                Web Developer
+            </p>
+
+            <div className="flex justify-around w-9/12 mx-auto my-5 text-orange md:w-full ">
+                <a href="https://github.com/makidai">
+                    <AiFillGithub className="w-8 h-8 cursor-pointer" />
+                </a>
+            </div>
+
+            <div
+                className="py-4 my-5 bg-gray-200 dark:bg-dark-200 dark:bg-black-500"
+                style={{ marginLeft: "-1rem", marginRight: "-1rem" }}
+            >
+                <div className="flex items-center justify-center">
+                    <GoLocation className="mr-2" /> <span>Tokyo,Japan </span>
+                </div>
+            </div>
+
+            <button className="w-8/12 px-5 py-2 my-4 text-white bg-black rounded-full cursor-pointer bg-gradient-to-r from-orange to-red focus:outline-none hover:scale-105 ">
+                Toggle Theme
+            </button>
+        </>
+    );
+};
+
+export default Sidebar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,32 @@
 module.exports = {
-  purge: [
-    './pages/**/*.tsx', './components/**/*.tsx'
-  ],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+    purge: ["./pages/**/*.tsx", "./components/**/*.tsx"],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        fontFamily: {
+            sans: ["ui-sans-serif", "system-ui"],
+            kaushan: ["Kaushan Script"],
+        },
+        extend: {
+            colors: {
+                orange: {
+                    DEFAULT: "#F38A15",
+                },
+                red: {
+                    DEFAULT: "#D2215D",
+                },
+                dark: {
+                    DEFAULT: "#010101",
+                    100: "#0a0b0e",
+                    200: "#16181d",
+                    300: "#16181d",
+                    500: "#0f1115",
+                    700: "#202125",
+                },
+            },
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+};
