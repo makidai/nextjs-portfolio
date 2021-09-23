ab545b08be9f651e492b03016fec682c4fbea295
Ryodai Makino <davinci0327@icloud.com>
2021-09-23 23:00:20 +0900

    create ServiceCard

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,31 @@
+import { services } from "../data";
+import ServiceCard from "../components/ServiceCard";
+
 const index = () => {
     return (
-        <div>
-            
+        <div className="flex flex-col flex-grow px-6 pt-1 ">
+            <h6 className="my-3 text-base font-medium">
+                都内のWeb系スタートアップでフルスタックエンジニア兼スクラムリーダーを担当しています.
+            </h6>
+            <div
+                className="flex-grow p-4 mt-5 bg-gray-400 dark:bg-dark-100 "
+                style={{ marginLeft: "-1.5rem", marginRight: "-1.5rem" }}
+            >
+                <h4 className="my-3 text-xl font-semibold tracking-wide">
+                    What I am doing
+                </h4>
+
+                <div className="grid gap-6 my-3 md:grid-cols-2">
+                    {services.map((service) => (
+                        <div
+                            className="col-span-2 p-2 bg-gray-200 rounded-lg dark:bg-dark-200 md:col-span-1 "
+                            key={service.title}
+                        >
+                            <ServiceCard service={service} />
+                        </div>
+                    ))}
+                </div>
+            </div>
         </div>
     );
 };
