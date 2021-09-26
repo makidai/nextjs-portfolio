84881469eb9cc3174391b171d5b7b1555f09e417
Ryodai Makino <davinci0327@icloud.com>
2021-09-27 00:05:26 +0900

    create Projects Component

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -12,11 +12,11 @@ const Sidebar = () => {
     return (
         <>
             <Image
-                src="/avatar.jpg"
+                src="/images/avatar.jpg"
                 alt="avatar"
                 className="mx-auto border rounded-full"
-                height="128px"
-                width="128px"
+                height="150px"
+                width="120px"
                 layout="intrinsic"
                 quality="100"
             />
@@ -24,9 +24,7 @@ const Sidebar = () => {
                 <span className="text-orange ">Ryodai</span> Makino
             </h3>
 
-            <p className="my-1 text-sm medium">
-                A Full Stack Developer
-            </p>
+            <p className="my-1 text-sm medium">A Full Stack Developer</p>
 
             <div className="flex justify-around w-9/12 mx-auto my-5 text-orange md:w-full ">
                 <a href="https://github.com/makidai">
@@ -45,7 +43,7 @@ const Sidebar = () => {
 
             <button
                 onClick={changeTheme}
-                className="w-8/12 px-5 py-2 my-4 text-white bg-black rounded-full cursor-pointer bg-gradient-to-r from-orange to-red focus:outline-none hover:scale-105"
+                className="w-8/12 px-3 py-2 my-4 text-white bg-black rounded-full cursor-pointer bg-gradient-to-r from-orange to-red focus:outline-none hover:scale-105 "
             >
                 Toggle Theme
             </button>
