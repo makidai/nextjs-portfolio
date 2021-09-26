25276507648b171dc32f7a71a96a1647199b4bba
Ryodai Makino <davinci0327@icloud.com>
2021-09-27 00:16:41 +0900

    fix toggle theme

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -45,7 +45,7 @@ const Sidebar = () => {
                 onClick={changeTheme}
                 className="w-8/12 px-3 py-2 my-4 text-white bg-black rounded-full cursor-pointer bg-gradient-to-r from-orange to-red focus:outline-none hover:scale-105 "
             >
-                Toggle Theme
+                {theme === 'light' ? 'Dark UI' : 'Light UI'}
             </button>
         </>
     );
