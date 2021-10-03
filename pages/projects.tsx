0b6f65b4758345dcb2124a686d99b7df7b58461c
Ryodai Makino <davinci0327@icloud.com>
2021-10-03 13:55:10 +0900

    fix height

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -27,7 +27,7 @@ const Projects = () => {
     return (
         <motion.div
             className="px-5 py-2 overflow-y-scroll "
-            style={{ height: "65vh" }}
+            style={{ height: "72vh" }}
             variants={pageAnimation}
             animate="visible"
             initial="hidden"
